1196c13e43305d190aaf823dbee8c293d554436e
Hiro527 <hiro527.dev@gmail.com>
2024-07-11 16:45:06 +0900

    update

--- setup.sh
+++ setup.sh
@@ -9,9 +9,6 @@ echo \
 "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin make -y
 
-git clone https://github.com/itkmaingit/YMovieHelperLocal.git
-cd YMovieHelperLocal
-sudo apt install -y make
 make init
